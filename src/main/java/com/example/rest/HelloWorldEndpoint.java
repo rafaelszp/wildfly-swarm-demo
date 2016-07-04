3bba2fc7853d5a92f277db4ca4e94a37e6db7304
Rafael Pereira <rafaelsouza@sistemafieg.org.br>
2016-07-04 14:06:18 -0300

    swagger-ui, not working

--- src/main/java/com/example/rest/HelloWorldEndpoint.java
+++ src/main/java/com/example/rest/HelloWorldEndpoint.java
@@ -1,6 +1,9 @@
 package com.example.rest;
 
+import io.swagger.annotations.Api;
+
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
@@ -8,13 +11,20 @@ import javax.ws.rs.Produces;
 
 @Path("/hello")
 @Produces(MediaType.APPLICATION_JSON)
+@Api(value="hello")
 public class HelloWorldEndpoint {
 
   @GET
   @Produces("text/plain")
   public Response doGet() {
     nada();
-    return Response.ok("Hello from WildFly Swarm! :D agora sim!").build();
+    return Response.ok("Hello from WildFly Swarm! :D agora sim! Como hot swap").build();
+  }
+
+  @GET
+  @Path("/{id: \\d+}")
+  public Response byId(@PathParam("id") Long id){
+    return Response.ok("Hello with id "+id).build();
   }
 
   public void nada(){
