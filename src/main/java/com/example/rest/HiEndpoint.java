c0588097367f6f2609275256bd44aff3fadc7565
Heiko Braun <ike.braun@googlemail.com>
2016-07-06 14:11:39 +0200

    Fix content types to make swagger UI work

--- src/main/java/com/example/rest/HiEndpoint.java
+++ src/main/java/com/example/rest/HiEndpoint.java
@@ -4,12 +4,15 @@ import io.swagger.annotations.Api;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
  * Created by rafael on 5/23/16.
  */
 @Path("/hi")
+@Produces(MediaType.TEXT_PLAIN)
 @Api(value="hi",description = "yet another salutation :D")
 public class HiEndpoint {
 
